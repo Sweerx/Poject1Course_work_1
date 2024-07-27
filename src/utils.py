a20783f76ffaabfb68ae1a8e1c477a7cf53cea23
Dmitry Yakushin <yakushin.dima8@gmail.com>
2024-07-27 20:05:22 +0300

    create func show_top_5_transactions, fetch_and_show_currency_rates

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,16 @@
-from datetime import datetime
+import datetime
+import json
+import os
 from typing import Any
 
 import pandas as pd
+import requests
+from dotenv import load_dotenv
+from requests import RequestException
 
+load_dotenv()
 
+api_key = os.getenv("API_KEY")
 
 def get_xlsx_data_dict(file_name: str) -> list[dict] | str:
     """Считывает данные о финансовых операциях из excel файла и преобразует их в список словарей"""
@@ -99,3 +106,55 @@ def show_cards(time_data: str, transactions: list | Any) -> list[dict]:
         card_dict["cashback"] = get_cashback_sum(total_spent)
         show_cards_list.append(card_dict)
     return show_cards_list
+
+
+def show_top_5_transactions(time_data: str, transactions: list | Any) -> list[dict[str, Any]]:
+    """Выводит информацию о 5 топ транзакциях по сумме платежа"""
+    for transaction in transactions:
+        neg_sum = transaction["payment_sum"]
+        transaction["payment_sum"] = abs(neg_sum)
+    month = time_data[5:7] + "." + time_data[:4]
+    month_transactions = []
+    for transaction in transactions:
+        date = str(transaction["payment_date"])
+        if date[3:] == month:
+            month_transactions.append(transaction)
+    sorted_transactions = sorted(
+        month_transactions,
+        key=lambda transaction: transaction["payment_sum"],
+        reverse=True,
+    )
+    list_index = 1
+    top_5_transactions = []
+    for transaction in sorted_transactions:
+        if list_index < 6:
+            top_transaction_dict = {}
+            top_transaction_dict["date"] = transaction["payment_date"]
+            top_transaction_dict["amount"] = transaction["payment_sum"]
+            top_transaction_dict["category"] = transaction["category"]
+            top_transaction_dict["description"] = transaction["description"]
+            top_5_transactions.append(top_transaction_dict)
+            list_index += 1
+    return top_5_transactions
+
+
+def fetch_and_show_currency_rates() -> list[dict[str, Any]]:
+    """Выводит курс валют и записывает из в файл .json"""
+    try:
+        url = "https://www.cbr-xml-daily.ru/daily_json.js"
+        #        payload = {}
+        headers = {"apikey": api_key}
+        response = requests.get(url, headers=headers)
+        print(response)
+        result = response.json()
+        print(result)
+        exchange_rates_list = []
+        usd_rate = {"currency": "USD", "rate": round(result["Valute"]["USD"]["Value"], 2)}
+        eur_rate = {"currency": "EUR", "rate": round(result["Valute"]["EUR"]["Value"], 2)}
+        exchange_rates_list.append(usd_rate)
+        exchange_rates_list.append(eur_rate)
+        with open("user_settings.json", "w") as f:
+            json.dump(exchange_rates_list, f)
+        return exchange_rates_list
+    except RequestException:
+        return [{}]
